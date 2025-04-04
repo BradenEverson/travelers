d700bbe3965ba50b77e2ef454f92c4a4e756f12b
BradenEverson <bradeneverson@gmail.com>
2025-04-04 04:43:31 -0500

    Create initial lang proj structure

---
+++ lang/build.zig
@@ -0,0 +1,18 @@
+// build.zig
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const exe = b.addExecutable(.{
+        .name = "traveler",
+        .root_source_file = b.path("src/main.zig"),
+        .target = b.graph.host,
+    });
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("run", "Run the interpreter");
+    run_step.dependOn(&run_cmd.step);
+}
