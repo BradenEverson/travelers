c96656b3d286717ae1c53ea70bffe388a06a20de
Braden Everson <bradeneverson@gmail.com>
2025-04-04 07:40:55 -0500

    Use a page allocator instead

--- lang/src/main.zig
+++ lang/src/main.zig
@@ -4,10 +4,8 @@ const tokenizer = @import("./tokenizer.zig");
 const ArrayList = std.ArrayList;
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
+    const allocator = std.heap.page_allocator;
 
-    std.debug.print("Makin a lang\n", .{});
     var stream_buf = ArrayList(tokenizer.Token).init(allocator);
     defer stream_buf.deinit();
 
