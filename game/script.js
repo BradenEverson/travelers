5ddc96030a63d8ca956d304840e987069fe1b3c4
BradenEverson <bradeneverson@gmail.com>
2025-04-10 05:36:33 -0500

    Oooooooo

--- game/script.js
+++ game/script.js
@@ -3,14 +3,28 @@ var memory = new WebAssembly.Memory({
     maximum: 2,
 });
 
+var x;
+var y;
+
 var importObject = {
     env: {
-        updatePosition: (x, y) => console.log("Moving to " + x + ", " + y), 
+        updatePosition: (new_x, new_y) => {
+            x = new_x;
+            y = new_y;
+
+            console.log(`New position is (${x}, ${y})`)
+        }, 
+        moveRelative: (dx, dy) => {
+            x += dx;
+            y += dy;
+
+            console.log(`New position is (${x}, ${y})`)
+        },
         memory: memory,
     },
 };
 
 WebAssembly.instantiateStreaming(fetch("wasm/traveler_wasm.wasm"), importObject).then((result) => {
     const wasmMemoryArray = new Uint8Array(memory.buffer);
-    result.instance.exports.move(1, 2);
+    result.instance.exports.moveRoutine();
 });
--- lang/src/wasm.zig
+++ lang/src/wasm.zig
@@ -1,7 +1,13 @@
 //! Main wasm runtime
 
 pub extern "env" fn updatePosition(x: u32, y: u32) void;
+pub extern "env" fn moveRelative(dx: i32, dy: i32) void;
 
-export fn move(x: u32, y: u32) void {
-    updatePosition(x, y);
+export fn moveRoutine() void {
+    updatePosition(16, 16);
+
+    moveRelative(5, 0);
+    moveRelative(-5, 0);
+    moveRelative(0, 10);
+    moveRelative(3, -2);
 }
