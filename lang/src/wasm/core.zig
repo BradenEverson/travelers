bbadeca1a5b29f64dbcb32d1d2a214206aed0429
Braden Everson <bradeneverson@gmail.com>
2025-04-14 14:41:01 -0500

    Trying to do some comptime stuff, issue in size

--- lang/src/wasm/core.zig
+++ lang/src/wasm/core.zig
@@ -1,7 +1,10 @@
 //! Core WASM module stuff
+const std = @import("std");
 
 extern "env" fn log_js(ptr: [*]const u8, len: usize) void;
 
-pub fn log(msg: []const u8) void {
-    log_js(msg.ptr, msg.len);
+pub fn log(comptime fmt: []const u8, args: anytype) void {
+    var buf: [fmt.len]u8 = {};
+    const string = std.fmt.bufPrint(&buf, fmt, args) catch unreachable;
+    log_js(string.ptr, string.len);
 }
