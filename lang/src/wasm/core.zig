4095f00234d1b95e0c5a09b99185421fa3f33a5c
BradenEverson <bradeneverson@gmail.com>
2025-04-15 06:33:55 -0500

    Eh using a buffer for now, comptime would be cool but whateva

--- lang/src/wasm/core.zig
+++ lang/src/wasm/core.zig
@@ -4,7 +4,7 @@ const std = @import("std");
 extern "env" fn log_js(ptr: [*]const u8, len: usize) void;
 
 pub fn log(comptime fmt: []const u8, args: anytype) void {
-    var buf: [fmt.len]u8 = {};
-    const string = std.fmt.bufPrint(&buf, fmt, args) catch unreachable;
+    var buf: [1024]u8 = undefined;
+    const string = std.fmt.bufPrint(&buf, fmt, args) catch @panic("log: formatting failed");
     log_js(string.ptr, string.len);
 }
