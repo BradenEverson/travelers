2e2d2a5a217c900ef58dc7c8968c4b2527b73487
BradenEverson <bradeneverson@gmail.com>
2025-04-28 13:44:56 -0500

    Start loopin

--- frontend/js/battle.js
+++ frontend/js/battle.js
@@ -17,7 +17,6 @@ let canvas, ctx;
 
 let spawnablePoints = [];
 
-
 let memory = new WebAssembly.Memory({
   initial: 2,
 });
@@ -149,153 +148,157 @@ function startGame() {
       return response.json();
     })
     .then((json) => {
-        const spawns = shuffleArray(spawnablePoints)
-        playerSpawn = spawns[0];
+      const spawns = shuffleArray(spawnablePoints);
+      playerSpawn = spawns[0];
 
+      x = playerSpawn[0];
+      y = playerSpawn[1];
 
-        x = playerSpawn[0];
-        y = playerSpawn[1];
-
-        let playerVtable = {
-            env: {
-                moveRelative: (dx, dy) => {
-                    if (
-                        (x == 0 && dx < 0) ||
-                        (y == 0 && dy < 0) ||
-                        (x == grid.x - 1 && dx > 0) ||
-                        (y == grid.y - 1 && dy > 0)
-                    ) {
-                        return -1;
-                    }
-
-                    const nx = x + dx;
-                    const ny = y + dy;
-
-                    if (tile_types[ny][nx] != OPEN && tile_types[ny][nx] != TRAP) {
-                        return -1;
-                    }
-
-                    x = nx;
-                    y = ny;
-
-                    if (tile_types[ny][nx] == TRAP) {
-                        tile_types[ny][nx] = OPEN;
-                        return -2;
-                    }
-
-                    return 0;
-                },
-
-                updateHealthBar: (hp) => {
-                    document.getElementById("hp").innerText = `Health: ${hp}/100`;
-                },
-
-                attackAt: (dx, dy) => {
-                    const nx = x + dx;
-                    const ny = y + dy;
-
-                    if (tile_types[ny][nx] == WOOD) {
-                        tile_types[ny][nx] = OPEN;
-                        return WOOD;
-                    } else if (tile_types[ny][nx] == ENEMY) {
-                        tile_types[ny][nx] = OPEN;
-                        return ENEMY;
-                    }
-
-                    return 0.0;
-                },
-
-                trapAt: (dx, dy) => {
-                    const nx = x + dx;
-                    const ny = y + dy;
-
-                    if (
-                        nx < 0 ||
-                        nx >= grid.x ||
-                        ny < 0 ||
-                        ny >= grid.y ||
-                        tile_types[ny][nx] != OPEN
-                    ) {
-                        return false;
-                    }
-
-                    tile_types[ny][nx] = TRAP;
-
-                    return true;
-                },
-
-                lookAtRelative: (dx, dy) => {
-                    const nx = x + dx;
-                    const ny = y + dy;
-
-                    if (nx < 0 || nx >= grid.x || ny < 0 || ny >= grid.y) {
-                        return -1;
-                    } else if (inStorm(nx, ny)) {
-                        return STORM;
-                    } else {
-                        return tile_types[y + dy][x + dx];
-                    }
-                },
-
-                log_js: (ptr, len) => {
-                    const buffer = new Uint8Array(memory.buffer, ptr, len);
-                    const str = new TextDecoder().decode(buffer);
-                    console.log(str);
-                },
-
-                memory: memory,
-
-            }
-        };
-
-        WebAssembly.instantiateStreaming(
-            fetch("wasm/traveler_wasm.wasm"),
-            playerVtable,
-        ).then((result) => {
-            const wasmMemoryArray = new Uint8Array(memory.buffer);
-
-            function stringToPtr(str) {
-                const encoder = new TextEncoder();
-                const encoded = encoder.encode(str);
-                const ptr = result.instance.exports.alloc(encoded.length);
-                const mem = new Uint8Array(memory.buffer);
-                mem.set(encoded, ptr);
-
-                return [ptr, encoded.length];
+      let playerVtable = {
+        env: {
+          moveRelative: (dx, dy) => {
+            if (
+              (x == 0 && dx < 0) ||
+              (y == 0 && dy < 0) ||
+              (x == grid.x - 1 && dx > 0) ||
+              (y == grid.y - 1 && dy > 0)
+            ) {
+              return -1;
             }
 
-            let stormTicks = 0;
+            const nx = x + dx;
+            const ny = y + dy;
 
-            function tickStorm() {
-                stormTicks += 1;
-
-                if (stormTicks % 20 == 0 && inStorm(x, y)) {
-                    result.instance.exports.doDamage(1);
-                }
+            if (tile_types[ny][nx] != OPEN && tile_types[ny][nx] != TRAP) {
+              return -1;
             }
 
-            const code = json.creator;
-            const [ptr, len] = stringToPtr(code);
-            const res = result.instance.exports.loadProgram(ptr, len);
+            x = nx;
+            y = ny;
 
-            setInterval(() => {
-                tickStorm();
-                result.instance.exports.step();
+            if (tile_types[ny][nx] == TRAP) {
+              tile_types[ny][nx] = OPEN;
+              return -2;
+            }
 
-                if (result.instance.exports.getHealth() == 0 && !dead) {
-                    dead = true;
-                    alert("You're dead :(");
-                }
-            }, 25);
-        });
+            return 0;
+          },
+
+          updateHealthBar: (hp) => {
+            document.getElementById("hp").innerText = `Health: ${hp}/100`;
+          },
+
+          attackAt: (dx, dy) => {
+            const nx = x + dx;
+            const ny = y + dy;
+
+            if (tile_types[ny][nx] == WOOD) {
+              tile_types[ny][nx] = OPEN;
+              return WOOD;
+            } else if (tile_types[ny][nx] == ENEMY) {
+              tile_types[ny][nx] = OPEN;
+              return ENEMY;
+            }
+
+            return 0.0;
+          },
+
+          trapAt: (dx, dy) => {
+            const nx = x + dx;
+            const ny = y + dy;
+
+            if (
+              nx < 0 ||
+              nx >= grid.x ||
+              ny < 0 ||
+              ny >= grid.y ||
+              tile_types[ny][nx] != OPEN
+            ) {
+              return false;
+            }
+
+            tile_types[ny][nx] = TRAP;
+
+            return true;
+          },
+
+          lookAtRelative: (dx, dy) => {
+            const nx = x + dx;
+            const ny = y + dy;
+
+            if (nx < 0 || nx >= grid.x || ny < 0 || ny >= grid.y) {
+              return -1;
+            } else if (inStorm(nx, ny)) {
+              return STORM;
+            } else {
+              return tile_types[y + dy][x + dx];
+            }
+          },
+
+          log_js: (ptr, len) => {
+            const buffer = new Uint8Array(memory.buffer, ptr, len);
+            const str = new TextDecoder().decode(buffer);
+            console.log(str);
+          },
+
+          memory: memory,
+        },
+      };
+
+      WebAssembly.instantiateStreaming(
+        fetch("wasm/traveler_wasm.wasm"),
+        playerVtable,
+      ).then((result) => {
+        const wasmMemoryArray = new Uint8Array(memory.buffer);
+
+        function stringToPtr(str) {
+          const encoder = new TextEncoder();
+          const encoded = encoder.encode(str);
+          const ptr = result.instance.exports.alloc(encoded.length);
+          const mem = new Uint8Array(memory.buffer);
+          mem.set(encoded, ptr);
+
+          return [ptr, encoded.length];
+        }
+
+        let stormTicks = 0;
+
+        function tickStorm() {
+          stormTicks += 1;
+
+          if (stormTicks % 20 == 0 && inStorm(x, y)) {
+            result.instance.exports.doDamage(1);
+          }
+        }
+
+        const code = json.creator;
+        const [ptr, len] = stringToPtr(code);
+        const res = result.instance.exports.loadProgram(ptr, len);
+
+        setInterval(() => {
+          tickStorm();
+          result.instance.exports.step();
+
+          if (result.instance.exports.getHealth() == 0 && !dead) {
+            dead = true;
+            alert("You're dead :(");
+          }
+        }, 25);
+      });
+
+        
+      for (let i = 0; i < json.others.length; i++) {
+        const newScript = json.others[i];
+        console.log(newScript);
+      }
+
     });
 
-            setInterval(updateStorm, 30000);
-            setInterval(tick, 25);
+  setInterval(updateStorm, 30000);
+  setInterval(tick, 25);
 }
 
 function tick() {
-  // TODO: WASM tick
   drawGrid();
 }
 
